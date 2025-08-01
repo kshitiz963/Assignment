423178e89a67c50f9fb7480a10bd1b8654ebbb91
Kshitiz Vardhan <kshitiz1963.be23@chitkara.edu.in>
2025-08-01 15:12:16 +0530

    Upload profile page

---
+++ ProfilePage/ProfilePage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfilePage
+//
+//  Created by Kshitiz on 15/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProfilePage/ProfilePageTests/ProfilePageTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfilePageTests.swift
+//  ProfilePageTests
+//
+//  Created by Kshitiz on 15/07/25.
+//
+
+import Testing
+@testable import ProfilePage
+
+struct ProfilePageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
