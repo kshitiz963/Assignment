1fdbee8ce3a53e7ffb9d2419faadb6b14c9dd0a4
Kshitiz Vardhan <kshitiz1963.be23@chitkara.edu.in>
2025-08-01 15:38:03 +0530

    uploaded Lab - About Me

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Kshitiz on 01/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
